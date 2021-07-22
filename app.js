c3f55db022baba06c4f186693fde24f87dd854d8
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-07-23 00:59:59 +0530

    Added shortcut keys for color buttons

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const newGridButton = document.querySelector("#new-grid");
 const blackButton = document.querySelector("#black-btn");
 const randomButton = document.querySelector("#random-btn");
 const colorPicker = document.querySelector("#color-picker");
+const body = document.querySelector("body");
 
 newGridButton.addEventListener('click', newGrid);
 generateGrid(16, 16);
@@ -12,13 +13,31 @@ blackButton.addEventListener('click', () => {
     hexCode = "#000";
 });
 
+body.addEventListener('keydown', function (e) {
+    if (e.key === 'b') {
+        hexCode = "#000";
+    }
+});
+
 randomButton.addEventListener('click', () => {
-    hexCode = "#"+Math.floor(Math.random()*16777215).toString(16);
+    hexCode = "#" + Math.floor(Math.random() * 16777215).toString(16);
+});
+
+body.addEventListener('keydown', function(e) {
+    if (e.key === 'r') {
+        hexCode = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    }
 });
 
 colorPicker.addEventListener('focus', () => {
     hexCode = colorPicker.value;
 });
+
+body.addEventListener('keypress', function(e) {
+    if (e.key === 'c') {
+        hexCode = colorPicker.value;
+    }
+})
 
 function generateGrid(rows, cols) {
     container.style.setProperty("--grid-rows", rows);
